8a182375b5ec70f18991dd83baa815b92d71b3e4
littlebai28 <39129920+littlebai28@users.noreply.github.com>
2022-04-22 20:23:12 -0400

    Add files via upload

---
+++ run_demo.sh
@@ -0,0 +1,3 @@
+python3 demo.py --is_only_data=True
+python3 mdn/save_mdn_gmm.py data/testimgs/
+python3 demo.py --is_only_data=False
---
+++ run_lfw.sh
@@ -0,0 +1,3 @@
+python3 vae/train.py lfw
+python3 mdn/mdn.py lfw
+python3 vae/test.py lfw 
